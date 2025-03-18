311e0a895e1d2037d75513bfbbf1fa9fc2c235df
Tim <>
2025-03-18 22:47:00 +0100

    Added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,15 @@
-import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
+import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "TimBlazic.dev - Full Stack Developer",
-  description: "Full stack developer portfolio showcasing projects and skills",
+  title: "Tim Blažič - Full Stack Developer",
+  description:
+    "🚀 High-Performance Web Solutions for startups & agencies. Fast, SEO-Optimized, and Designed for Conversions. I transform complex problems into elegant, efficient solutions.",
 };
 
 export default function RootLayout({
@@ -17,7 +19,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={cn(inter.className, "bg-background")}>{children}</body>
+      <body className={cn(inter.className, "bg-background")}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
