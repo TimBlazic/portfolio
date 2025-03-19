b6b184c1dfcf4500e928698827c5cb72e09e1d09
Tim <>
2025-03-19 08:13:11 +0100

    Small changes

--- app/page.tsx
+++ app/page.tsx
@@ -17,13 +17,11 @@ export default function Page() {
     <div className="min-h-screen bg-background">
       <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
         <div className="container mx-auto max-w-7xl flex h-14 items-center px-4">
-          <div className="mr-4 hidden md:flex">
+          <div className="mr-4 flex">
             <Link className="mr-6 flex items-center space-x-2" href="/">
-              <span className="hidden font-bold sm:inline-block">
-                TimBlazic.dev
-              </span>
+              <span className=" font-bold">TimBlazic.dev</span>
             </Link>
-            <nav className="flex items-center space-x-6 text-sm font-medium">
+            <nav className="items-center  hidden md:flex space-x-6 text-sm font-medium">
               <Link
                 href="#about"
                 className="transition-colors hover:text-foreground/80"
@@ -44,6 +42,7 @@ export default function Page() {
               </Link>
             </nav>
           </div>
+
           <Button className="ml-auto" asChild>
             <Link href="#contact">Get in Touch</Link>
           </Button>
